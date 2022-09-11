8e40333d7639cd12565c7a6c21d26e8c3a5e5ca0
Udeh Ifeanyi <ifescoguy@gmail.com>
2022-09-11 02:32:42 +0100

    fourth commit

--- scrips/script.js
+++ scrips/script.js
@@ -12,11 +12,11 @@ const scrollElements = document.querySelectorAll(".scroll");
 
 const elementInView = (el, dividend = 1) => {
   const elementTop = el.getBoundingClientRect().top;
-
+ 
   return (
     elementTop <=
     (window.innerHeight || document.documentElement.clientHeight) / dividend
-  );
+  ); 
 };
 
 const elementOutofView = (el) => {
